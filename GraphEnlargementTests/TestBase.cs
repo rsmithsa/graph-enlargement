d7141b4303d30e68f16ccc4c862d8c593d0b0323
rsmithsa <richard@smith-sa.com>
2017-01-14 16:12:37 +0200

    Test project

---
+++ GraphEnlargementTests/TestBase.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestBase.cs" company="Richard Smith">
+//     Copyright (c) Richard Smith. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GraphEnlargementTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public abstract class TestBase
+    {
+    }
+}
